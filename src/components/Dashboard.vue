b2cebd542f2bee353d83227eb38727b9797e3001
jnahas55 <jorge.nahas55@gmail.com>
2018-08-11 20:27:18 -0300

    Adding changes in Dashboard view

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,11 +3,11 @@
   <!-- div for Dashboard view -->
   <div v-if="renderDashboardView" id="dashboard" style="margin: 1.5%;">
 
-    <div class="row" style="font-family: sans-serif;">
+    <div class="row d-flex align-items-stretch" style="font-family: sans-serif;">
 
-      <div class="col-lg-8">
-        <!-- Example Pie Chart Card-->
-        <div class="card mb-3 shadow">
+      <div class="col-md-8">
+
+        <div class="card mb-3 shadow h-100">
           <div class="card-header">
             <i class="fa fa-pie-chart"></i> Most Executed Triggers
           </div>
@@ -17,27 +17,59 @@
                 <canvas id="barChart" class="chartjs-render-monitor"></canvas>
               </div>
               <div class="col-md-6">
-                <canvas id="percentageBar" class="chartjs-render-monitor"></canvas>
+                <div class="row">
+                  <div class="col-md-6">
+                    <!--canvas id="myPieChart2" class="chartjs-render-monitor"></canvas-->
+                    <canvas id="percentageBar" class="chartjs-render-monitor"></canvas>
+                    <!--h3 class="card-title text-primary">7</h3-->
+                  </div>
+                  <div class="col-md-6 text-center my-auto">
+                    <div class="h5 mb-0 text-primary">33.33%</div>
+                    <div class="small text-muted">Data Point Registration</div>
+                    <hr>
+                    <div class="h5 mb-0 text-success">66.66%</div>
+                    <div class="small text-muted">Time Interval</div>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="col-lg-4">
+      <div class="col-md-4">
         <!-- Example Pie Chart Card-->
-        <div class="card mb-3 shadow">
+        <div class="card mb-3 shadow h-100">
           <div class="card-header">
             <i class="fa fa-pie-chart"></i> Most Triggered Actions
           </div>
           <div class="card-body">
-            <canvas id="myPieChart2" class="chartjs-render-monitor"></canvas>
+            <div class="row">
+              <div class="col-md-6">
+                <canvas id="myPieChart2" class="chartjs-render-monitor"></canvas>
+                <!--h3 class="card-title text-primary">7</h3-->
+              </div>
+              <div class="col-md-6 text-center my-auto">
+                <div class="h5 mb-0 text-primary">9</div>
+                <div class="small text-muted">Action 1</div>
+                <hr>
+                <div class="h5 mb-0 text-warning">7</div>
+                <div class="small text-muted">Action 2</div>
+                <hr>
+                <div class="h5 mb-0 text-success">5</div>
+                <div class="small text-muted">Action 3</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      <!--/div-->
+
     </div>
 
-    <div class="row" style="font-family: sans-serif;">
+    <div class="row" style="font-family: sans-serif; margin-top: 2%;">
 
       <!--data-stream-card v-for="dS in dataStreams" v-bind:name="dS.name" v-bind:value="dS.current_value" v-bind:timestamp="dS.last_updated" @click.native="assignDataStream(dS)"></data-stream-card>
       <div v-if="showAddDataStream" class="col-md-2">
@@ -62,7 +94,10 @@
           <div class="sk-circle12 sk-child"></div>
         </div>
 
-        <div v-if="showAddDataStream" class="card shadow" style="font-family: sans-serif;">
+        <div v-if="showAddDataStream" class="card shadow h-100" style="font-family: sans-serif;">
+          <div class="card-header">
+            <i class="fa fa-pie-chart"></i> Most Recently Updated Streams
+          </div>
           <div class="card-body">
             <table class="table table-striped table-responsive table-sm" style="width:100%;">
               <thead>
@@ -87,24 +122,49 @@
       </div>
 
       <div class="col-md-4" style="font-family: sans-serif;">
-        <div class="card shadow" style="margin-bottom: 4%;">
-          <div class="card-body text-center">
-            <h3 class="card-title text-primary">31</h3>
-            <p class="card-text">Actions</p>
-          </div>
-        </div>
-        <div class="card shadow" style="margin-bottom: 4%;">
-          <div class="card-body text-center">
-            <h3 class="card-title text-primary">12</h3>
-            <p class="card-text">Triggers</p>
-          </div>
-        </div>
+
         <div class="card shadow">
           <div class="card-body text-center">
-            <h3 class="card-title text-primary">9</h3>
-            <p class="card-text">Data Streams</p>
+            <div class="row">
+              <div class="col-sm-6">
+                <h3 class="card-title text-primary">12</h3>
+              </div>
+              <div class="col-sm-6 text-center">
+                <div class="text-muted">Data Streams</div>
+              </div>
+            </div>
           </div>
         </div>
+
+
+        <div class="card shadow my-5"><!-- my-5 da margen en el eje Y : 1.5 rem -->
+          <div class="card-body text-center">
+            <div class="row">
+              <div class="col-sm-6">
+                <h3 class="card-title text-primary">23</h3>
+              </div>
+              <div class="col-sm-6 text-center">
+                <div class="text-muted">Actions</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card shadow">
+          <div class="card-body text-center">
+            <div class="row">
+              <div class="col-sm-6">
+                <h3 class="card-title text-primary">7</h3>
+              </div>
+              <div class="col-sm-6 text-center">
+                <div class="text-muted">Triggers</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
       </div>
 
     </div>
@@ -135,7 +195,7 @@ export default {
           display: false
         },
         responsive:true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: true,
         title: {
           display: false,
           text: 'Most Executed actions (times)'
@@ -144,7 +204,11 @@ export default {
           xAxes: [{
             gridLines: {
               display:false
-            }
+            },
+            ticks: {
+              display: false //this will remove only the label
+            },
+            display: false
           }],
           yAxes: [{
             gridLines: {
@@ -159,21 +223,21 @@ export default {
     new Chart(ctx, {
       type: 'doughnut',
       data: {
-        labels: ['Action 1', 'Action 2', 'Action 3', 'Action 4'],
+        labels: ['Action 1', 'Action 2', 'Action 3'],
         datasets: [{
-          data: [11, 22, 5, 17],
-          backgroundColor: ['#FABB3C', '#32D75E','#D02FC0','#EB0524']
+          data: [7, 9, 5],
+          backgroundColor: ['#ffc107', '#007bff', '#28a745']
           /*backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f','#e8c3b9','#c45850']*/
         }]
       },
       options: {
-        /*legend: {
+        legend: { // Para no mostrar la leyenda de cada set de datos
             display: false
-        },*/
+        },
         title: false,
         responsive:true,
         cutoutPercentage:0,
-        maintainAspectRatio: true,
+        maintainAspectRatio: false,
         title: {
           display: false,
           text: 'Most Executed actions (times)'
@@ -192,13 +256,13 @@ export default {
         }]
       },
       options: {
-        /*legend: {
+        legend: {
             display: false
-        },*/
+        },
         title: false,
         responsive:true,
         cutoutPercentage:70,
-        maintainAspectRatio: true,
+        maintainAspectRatio: false,
       }
     });
 

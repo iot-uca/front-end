a44279c681d8e7a26bc3cf3738db18a2f329198d
jnahas55 <jorge.nahas55@gmail.com>
2020-06-24 02:48:30 -0300

    #6 Adding ENTRYPOINT for Dockerfile

--- initialization.sh
+++ initialization.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-COSMOS_LOCATION="$1";
-echo COSMOS_LOCATION is $COSMOS_LOCATION;
-export COSMOS_LOCATION;
-nginx -g daemon off;
+export COSMOS_LOCATION=$cosmos_location
+echo "COSMOS_LOCATION is $COSMOS_LOCATION"
+nginx -g daemon off

12f80c15e4ba058279945dda8826daa41f08c237
jnahas55 <jorge.nahas55@gmail.com>
2020-06-24 20:12:44 -0300

    #13 Changing routes behavior

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,16 @@
 
     <burguer-menu></burguer-menu>
     <add-stream-modal></add-stream-modal>
-    <edit-stream-modal></edit-stream-modal>
-
-    <add-command-modal></add-command-modal>
-
+    <add-command-modal></add-command-modal
     <remove-elements-modal></remove-elements-modal>
     <add-action-modal></add-action-modal>
     <show-action-modal></show-action-modal>
-    <edit-action-modal></edit-action-modal>
-
     <add-trigger-modal></add-trigger-modal>
     <data-points-modal></data-points-modal>
     <status-modal></status-modal>
-
-
     <show-trigger-modal></show-trigger-modal>
-
     <router-view></router-view>
-
     <add-data-point-modal></add-data-point-modal>
-
-
-    <!-- FORM VALIDATIONS IN VUE JS => https://vuejs.org/v2/cookbook/form-validation.html -->
-
-    <!--  SPINNER VERY COSMOS STYLE https://epic-spinners.epicmax.co/#/  -->
-
-    <!--  Dependencies defined in package-lock.json 907  -->
-    <!--  Dependencies defined in package.json 57 -->
 
   </div>
 </template>
@@ -37,26 +20,17 @@
 
 <script>
   import BurguerMenu from './components/BurguerMenu.vue'
-  import SecurityView from './components/SecurityView.vue'
   import AddStreamModal from './components/AddStreamModal.vue'
   import Dashboard from './components/Dashboard.vue'
-  import EditStreamModal from './components/EditStreamModal.vue'
   import RemoveElementsModal from './components/RemoveElementsModal.vue'
   import DataPointsModal from './components/DataPointsModal.vue'
   import AddActionModal from './components/AddActionModal.vue'
   import ShowActionModal from './components/ShowActionModal.vue'
-  import EditActionModal from './components/EditActionModal.vue'
   import AddTriggerModal from './components/AddTriggerModal.vue'
   import ShowTriggerModal from './components/ShowTriggerModal.vue'
   import AddCommandModal from './components/AddCommandModal.vue'
   import SuccessFailureModal from './components/SuccessFailureModal.vue'
   import AddDataPointModal from './components/AddDataPointModal.vue'
-  //import AboutView from './components/AboutView.vue'
-
-
-  //import Spinner from './components/Spinner.vue'
-
-  //import Chart from 'chart.js';
 
   export default {
 
@@ -64,42 +38,18 @@
     components: {
       'status-modal' : SuccessFailureModal,
       'add-trigger-modal': AddTriggerModal,
-      'edit-action-modal': EditActionModal,
       'show-action-modal': ShowActionModal,
       'add-action-modal': AddActionModal,
       'data-points-modal': DataPointsModal,
       'remove-elements-modal': RemoveElementsModal,
-      'edit-stream-modal' : EditStreamModal,
       'dashboard': Dashboard,
       'add-stream-modal': AddStreamModal,
       'show-trigger-modal': ShowTriggerModal,
-      'security-view': SecurityView,
       'burguer-menu': BurguerMenu,
       'add-command-modal' : AddCommandModal,
       'add-data-point-modal': AddDataPointModal,
   },
   mounted () {
-
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-      'use strict';
-
-      window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-          form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-          }, false);
-        });
-      }, false);
-    })();
 
   },
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import Dashboard from './components/Dashboard.vue';
-import SecurityView from './components/SecurityView.vue';
-import ActionView from './components/ActionView.vue';
-import TriggerView from './components/TriggerView.vue';
 import DataStreamView from './components/DataStreamView.vue';
 import CommandsView from './components/CommandsView.vue';
+import ActionView from './components/ActionView.vue';
+import TriggerView from './components/TriggerView.vue';
 import AboutView from './components/AboutView.vue'
 
 
@@ -11,10 +10,9 @@ import AboutView from './components/AboutView.vue'
 export default [
   { path: '/' , component: Dashboard },
   { path: '/dashboard' , component: Dashboard },
-  { path: '/security' , component: SecurityView },
   { path: '/dataStreams' , component: DataStreamView },
-  { path: '/actions' , component: ActionView },
   { path: '/commands' , component: CommandsView },
+  { path: '/actions' , component: ActionView },
   { path: '/triggers' , component: TriggerView },
   { path: '/about' , component: AboutView },
 
